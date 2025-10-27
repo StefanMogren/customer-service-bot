f9012d914c79b3501e1bd9558b4d11606752777d
Stefan Mogren <stefan_mogren@hotmail.com>
2025-10-27 14:02:32 +0100

    Copied existing chat bot into project, added TechNova document

---
+++ uploadToSupabase/index.mjs
@@ -0,0 +1,35 @@
+import { readFile } from "fs/promises";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from "@supabase/supabase-js";
+
+import "dotenv/config";
+
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+
+import { OllamaEmbeddings } from "@langchain/ollama";
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
+
+try {
+	const text = await readFile(
+		`${process.cwd()}/TechNova AB – FAQ & Policydokument.txt`,
+		"utf-8"
+	);
+
+	const text_splitter = new RecursiveCharacterTextSplitter({
+		chunkSize: 200,
+		separators: ["\n\n", "\n"],
+		chunkOverlap: 50,
+	});
+
+	const splittedText = await text_splitter.createDocuments([text]);
+	const supabaseClient = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+	await SupabaseVectorStore.fromDocuments(
+		splittedText,
+		new OllamaEmbeddings({ model: "nomic-embed-text:latest" }),
+		{ client: supabaseClient, tableName: "technova_documents" }
+	);
+} catch (error) {
+	console.log(error);
+}
